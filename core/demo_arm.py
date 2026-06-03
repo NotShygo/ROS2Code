b44f5056a0bcb4d8d9c75eb4511d42aa883b366d
Antony <75847517+NotShygo@users.noreply.github.com>
2026-06-03 16:21:00 +0800

    Update demo_arm.py

--- core/demo_arm.py
+++ core/demo_arm.py
@@ -4,12 +4,14 @@ from Gripper2 import Gripper2
 
 if __name__ == "__main__":
     rclpy.init()
-
+    # Check param by 
+    #ros2 node list
+    # ros2 param get /move_group robot_description_semantic (replace /move_group by the node name)
     arm = RoboticArm2(
         group_name="panda_arm",
         base_frame="panda_link0",
-        end_effector_link="panda_link8",   # "panda_hand_tcp" or "panda_link8"
-        joint_names=[                         # << explicit, prevents the 9-joint bug
+        end_effector_link="panda_link8",
+        joint_names=[
             "panda_joint1","panda_joint2","panda_joint3",
             "panda_joint4","panda_joint5","panda_joint6","panda_joint7",
         ],
@@ -17,7 +19,7 @@ if __name__ == "__main__":
 
     gripper = Gripper2(
         backend="moveit",
-        group_name="hand",                    # try "panda_hand" if "hand" is rejected
+        group_name="hand",
         joint_names=["panda_finger_joint1", "panda_finger_joint2"],
         open_position=[0.04, 0.04],
         closed_position=[0.0, 0.0],
