5885bd9b58041163c6f786e5454eb7d2905dd52b
Antony <75847517+NotShygo@users.noreply.github.com>
2026-06-03 16:21:13 +0800

    Update demo_arm.py

--- core/demo_arm.py
+++ core/demo_arm.py
@@ -26,7 +26,6 @@ if __name__ == "__main__":
     )
 
     try:
-        # Joint goal (7 values for Panda)
         arm.move_to_joint([0.0, -0.785, 0.0, -2.356, 0.0, 1.571, 0.785])
 
         pose = arm.get_current_pose()
